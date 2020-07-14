56994eb35b5aa0d32c7ad2b9fe10fd4621be48eb
Kevin Clelland <kevinclelland@gmail.com>
2020-07-14 07:06:27 +0200

    Fixed logo and extracted css from html

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 module.exports = function(config) {
 
     config.addPassthroughCopy("src/js");
-    config.addPassthroughCopy("src/img");
+    config.addPassthroughCopy("src/assets/img");
+    config.addPassthroughCopy("src/assets/css");
     return  {
+      passthroughFileCopy: true,
       dir: {
         input: "src",
         output: "dist",
